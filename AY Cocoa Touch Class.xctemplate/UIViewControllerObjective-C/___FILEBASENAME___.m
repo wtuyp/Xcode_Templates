81b51bb3128bef43210a60d6ce2c353734f861da
Alpha Yu <wtuyp@163.com>
2019-03-14 11:49:32 +0800

    update

--- AY Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
+++ AY Cocoa Touch Class.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -10,11 +10,13 @@
 
 }
 
-- (void)viewDidLoad {
+#pragma mark - Life Cycle
+- (void)viewDidLoad 
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
-    // self.title = @"";
+    //self.title = @"";
 
     //数据
 
@@ -25,53 +27,67 @@
 }
 
 /*
-- (void)viewWillAppear:(BOOL)animated {
+- (void)viewWillAppear:(BOOL)animated 
+{
     [super viewWillAppear:animated];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
+- (void)viewDidAppear:(BOOL)animated 
+{
     [super viewDidAppear:animated];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
+- (void)viewWillDisappear:(BOOL)animated 
+{
     [super viewWillDisappear:animated];
 }
 
--(void)viewDidDisappear:(BOOL)animated {
+-(void)viewDidDisappear:(BOOL)animated 
+{
     [super viewDidDisappear:animated];
 }
 
-- (void)dealloc {
+- (void)dealloc 
+{
 }
 */
 
-#pragma mark - Setter Getter Methods
+#pragma mark - requests
 
-#pragma mark - Requests
-
-#pragma mark - Button Actions
-
-#pragma mark - Notification
-
-#pragma mark - UITableViewDelegate, UITableViewDataSource
+#pragma mark - UITableViewDataSource, UITableViewDelegate
 /*
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView 
+{
     return <#section#>;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section 
+{
     return <#row#>;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return <# UITableViewCell #>;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    return <#UITableViewCell#>;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
     return <#height#>;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
 }
 */
 
-#pragma mark - Private Methods
+#pragma mark - setter getter methods
+
+#pragma mark - button actions
+
+#pragma mark - notification
+
+#pragma mark - private methods
 
 @end
