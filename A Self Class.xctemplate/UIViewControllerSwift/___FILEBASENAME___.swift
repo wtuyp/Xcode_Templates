ef2252910df20fc6f8b6376055617d45b401018a
Alpha Yu <wtuyp@163.com>
2021-12-23 14:26:00 +0800

    update

--- A Self Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
+++ A Self Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -4,21 +4,60 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
 
+    //MARK: life
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = ""
+        
+        setupData()
+        setupNavigationBarItems()
+        setupSubviews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: override
+    
+    
+    //MARK: data
+    
+    func setupData() {
+        
     }
-    */
-
+    
+    
+    //MARK: view
+    
+    func setupNavigationBarItems() {
+//        let rightBarButtonItem = UIBarButtonItem(customView: <#T##UIView#>)
+//        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func setupSubviews() {
+        
+    }
+    
+    //MARK: getter
+    
+    
+    //MARK: setter
+    
+    
+    //MARK: action
+    
+    
+    //MARK: api
+    
+    
+    //MARK: notification
+    
+    
+    //MARK: public
+    
+    
+    //MARK: private
+    
+   
+    //MARK: other
+    
+    
 }
--- A Self Class.xctemplate/UIViewSwift/___FILEBASENAME___.swift
+++ A Self Class.xctemplate/UIViewSwift/___FILEBASENAME___.swift
@@ -4,12 +4,49 @@ import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: life
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupInit()
     }
-    */
-
+    
+    override init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupInit()
+    }
+    
+    func setupInit() {
+        
+    }
+    
+    //MARK: override
+    
+    
+    //MARK: getter
+    
+    
+    //MARK: setter
+    
+    
+    //MARK: action
+    
+    
+    //MARK: api
+    
+    
+    //MARK: notification
+    
+    
+    //MARK: public
+    
+    
+    //MARK: private
+    
+    
+    //MARK: other
+    
+    
 }
